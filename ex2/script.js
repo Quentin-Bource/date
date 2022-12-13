2ce757345a45f5e9b9ab6496730a38881e672067
Quentin <quentin01.bource@gmail.com>
2022-12-13 12:14:29 +0100

    ex3

--- ex2/script.js
+++ ex2/script.js
@@ -7,3 +7,16 @@ console.log( Math.floor((today.getTime() - bd.getTime()) / (1000 * 60 * 60 *24))
 
 // Ex 2.2 
 
+
+document.getElementById('check').onclick = function ()
+{
+    let vieux = new Date ("1970-01-01")
+    let date = document.getElementById('start').value
+    let ndate = new Date (date)
+
+    console.log(vieux)
+    console.log(date)
+
+    alert("Il y a " + Math.floor((ndate.getTime() - vieux.getTime()) / (1000 * 60 * 60 *24)) + " jour de différence entre le 1/01/1970 et ta date. ")
+
+}
